2e6dfc9c90f9df54ab33dc149070cb73d8ba23c6
xblackbagx-pixel <xblackbagx@gmail.com>
2026-04-01 17:36:06 -0400

    Update BULKINSERT_INTO_TABLES_PROC.sql

--- Scripts/BULKINSERT_INTO_TABLES_PROC.sql
+++ Scripts/BULKINSERT_INTO_TABLES_PROC.sql
@@ -1,70 +1,101 @@
+EXEC Bronze.Load_Bronze
 
 CREATE OR ALTER PROCEDURE Bronze.load_bronze AS 
 
 BEGIN
+	PRINT'================================================================'
+	PRINT 'Loading Bronze Layer'
+	PRINT'================================================================'
 
---First Example of BULK INSERT
-TRUNCATE TABLE bronze.crm_cust_info;
+	PRINT '------------------------------------------------------------------------';
+	PRINT'Loading CRM Tables';
+	PRINT '------------------------------------------------------------------------';
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	 FIRSTROW = 2,
-	 FIELDTERMINATOR = ',',
-	 TABLOCK 
-);
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
 
---Using example to create my own scripts for other tables
+	--First Example of BULK INSERT
+	TRUNCATE TABLE bronze.crm_cust_info;
 
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
 
-TRUNCATE TABLE bronze.crm_prd_info;
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		 FIRSTROW = 2,
+		 FIELDTERMINATOR = ',',
+		 TABLOCK 
+	);
 
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	 FIRSTROW = 2,
-	 FIELDTERMINATOR = ',',
-	 TABLOCK 
-);
+	--Using example to create my own scripts for other tables
 
-TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
 
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	 FIRSTROW = 2,
-	 FIELDTERMINATOR = ',',
-	 TABLOCK 
-);
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-TRUNCATE TABLE bronze.erp_CUST_AZ12;
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
 
-BULK INSERT bronze.erp_CUST_AZ12
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		 FIRSTROW = 2,
+		 FIELDTERMINATOR = ',',
+		 TABLOCK 
+	);
 
-TRUNCATE TABLE bronze.erp_LOC_A101;
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
 
-BULK INSERT bronze.erp_LOC_A101
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		 FIRSTROW = 2,
+		 FIELDTERMINATOR = ',',
+		 TABLOCK 
+	);
 
-TRUNCATE TABLE bronze.erp_PX_CAT_G1V2;
+	PRINT '------------------------------------------------------------------------';
+	PRINT'Loading ERP Tables';
+	PRINT '------------------------------------------------------------------------';
 
-BULK INSERT bronze.erp_PX_CAT_G1V2
-FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
+
+	TRUNCATE TABLE bronze.erp_CUST_AZ12;
+
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
+
+	BULK INSERT bronze.erp_CUST_AZ12
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
+
+	TRUNCATE TABLE bronze.erp_LOC_A101;
+
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
+
+	BULK INSERT bronze.erp_LOC_A101
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>>Truncating Table: bronze.crm_cust_info';
+
+	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2;
+
+	PRINT '>>Inserting Data Into: Bronze.crm_cust_info';
+
+	BULK INSERT bronze.erp_PX_CAT_G1V2
+	FROM 'C:\STUDY TOPICS\DataWithBaraaSQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 END 
