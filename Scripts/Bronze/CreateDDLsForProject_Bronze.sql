4dab12d83399957111197de73293d5c875547b69
xblackbagx-pixel <xblackbagx@gmail.com>
2026-04-19 19:45:36 -0400

    Create and update tables in Bronze schema
    Dropped existing tables if they exist and created new tables for customer, product, sales details, and other related information with updated data types.

--- Scripts/Bronze/CreateDDLsForProject_Bronze.sql
+++ Scripts/Bronze/CreateDDLsForProject_Bronze.sql
@@ -1,17 +1,33 @@
 USE DataWarehouse
+	
+IF OBJECT_ID ('Bronze.crm_cust_info', 'U') IS NOT NULL 
+	DROP TABLE Bronze.crm_cust_info
+CREATE TABLE Bronze.crm_cust_info ( 
+cst_id INT, 
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50), 
+cst_create_date DATE
+); 
 
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL 
+	DROP TABLE bronze.crm_prd_info
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 
 );
 
-CREATE TABLE Bronze.Sales_details (
+IF OBJECT_ID ('Bronze.crm_sales_details', 'U') IS NOT NULL 
+	DROP TABLE Bronze.crm_sales_details
+CREATE TABLE Bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
@@ -23,31 +39,28 @@ CREATE TABLE Bronze.Sales_details (
 	sls_price INT
 );
 
+IF OBJECT_ID ('Bronze.CUST_AZ12', 'U') IS NOT NULL 
+	DROP TABLE Bronze.CUST_AZ12
 CREATE TABLE Bronze.CUST_AZ12 (
 	CID NVARCHAR(50),
 	BDATE DATE,
 	GEN NVARCHAR(50)
 );
 
+IF OBJECT_ID ('Bronze.LOC_A101', 'U') IS NOT NULL 
+	DROP TABLE Bronze.LOC_A101
 CREATE TABLE Bronze.LOC_A101 (
 	CID NVARCHAR(50),
 	CNTRY NVARCHAR(50)
 );
 
+IF OBJECT_ID ('Bronze.PX_CAT_G1V2', 'U') IS NOT NULL 
+	DROP TABLE Bronze.PX_CAT_G1V2
 CREATE TABLE Bronze.PX_CAT_G1V2 (
 	ID NVARCHAR(50),
 	CAT NVARCHAR(50),
 	SUBCAT NVARCHAR(50),
 	Maintenance NVARCHAR(50)
 );
-
-ALTER TABLE bronze.crm_prd_info
-ALTER COLUMN prd_start_dt DATETIME;
-
-
-ALTER TABLE bronze.crm_prd_info
-ALTER COLUMN prd_end_dt DATETIME;
-
-EXEC sp_rename 'Bronze.Sales_details', 'crm_sales_details';
 
 --Change the names of all tables to equal Baraa Data 
