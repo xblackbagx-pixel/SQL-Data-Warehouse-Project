688bef05fe57e9e1cd5cb46315bcb73ca7625851
xblackbagx-pixel <xblackbagx@gmail.com>
2026-04-19 19:42:20 -0400

    Add cst_firstname column to crm_cust_info table

--- Scripts/Bronze/Bronze.crm_cust_info.sql
+++ Scripts/Bronze/Bronze.crm_cust_info.sql
@@ -3,6 +3,7 @@ USE DataWareHouse
 CREATE TABLE Bronze.crm_cust_info ( 
 cst_id INT, 
 cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
 cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50), 
