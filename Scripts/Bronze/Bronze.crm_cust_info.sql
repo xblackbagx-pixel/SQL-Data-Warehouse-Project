ee3da14272de3b054a6d8c75e8f6fab3c2aa093c
xblackbagx-pixel <xblackbagx@gmail.com>
2026-04-19 19:40:24 -0400

    Update Bronze.crm_cust_info.sql

--- Scripts/Bronze/Bronze.crm_cust_info.sql
+++ Scripts/Bronze/Bronze.crm_cust_info.sql
@@ -4,6 +4,7 @@ CREATE TABLE Bronze.crm_cust_info (
 cst_id INT, 
 cst_key NVARCHAR(50),
 cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50), 
 cst_create_date DATE
 ); 
