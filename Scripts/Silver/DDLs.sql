acc9d36f3d672b68ebcb173880a3a71a2ee0d5f0
xblackbagx-pixel <xblackbagx@gmail.com>
2026-04-19 19:35:24 -0400

    Update DDLs.sql

--- Scripts/Silver/DDLs.sql
+++ Scripts/Silver/DDLs.sql
@@ -7,7 +7,7 @@ CREATE TABLE Silver.crm_cust_info (
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50), 
-	cst_martial_status NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date NVARCHAR(50), 
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
